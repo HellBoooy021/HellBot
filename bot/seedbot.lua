e42f1199c89202374c79802fd96a6e5d92fee238
HellBoooy021 <davoodmohebbi021@gmail.com>
2016-02-06 22:38:00 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {107264463,144132967,0,tonumber(our_id)},--Sudo users
+    sudo_users = {101302929,144132967,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
